f31edd63979f037cd17f09b098bd14e87b29c87e
RD2023Zzz <b00154780@mytudublin.ie>
2024-04-06 17:07:13 +0100

    Validation for email and address.
    Form validation for email and address implemented. Comments added and parts of code modularised.

--- electronicDojo/register.php
+++ electronicDojo/register.php
@@ -1,4 +1,5 @@
 <?php
+//Initialize error messages
 $errorMessages = [];
 
     if (isset($_POST['submit'])){
@@ -51,6 +52,8 @@ $errorMessages = [];
                                 </div>
                                 <div class="form-group">
                                     <input type="email" class="form-control" id="email" placeholder="Email" name="email" required>
+                                    <?php if (!empty($errorMessages['emailError'])) {
+                                        echo '<span class="error">' . $errorMessages['emailError'] . '</span><br><br>';}?>
                                 </div>
                                 <div class="form-group">
                                     <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
@@ -59,6 +62,8 @@ $errorMessages = [];
                                 </div>
                                 <div class="form-group">
                                     <input type="text" class="form-control" id="address" placeholder="Address" name="address" required>
+                                    <?php if (!empty($errorMessages['addressError'])) {
+                                        echo '<span class="error">' . $errorMessages['addressError'] . '</span><br><br>';}?>
                                 </div>
                                 <div class="d-flex flex-row align-items-center justify-content-between">
                                     <h4>Already have an account? <a href="login.php">Login</a></h4>
